b47ef5e4fb45678803d591881bf95ff967217adf
Roberto Blanco <krull2001@hotmail.com>
2018-08-29 10:04:47 +0200

    minor changes

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -9,14 +9,16 @@ class AboutMe extends Component {
       <article className="topOfComponent">
         <div className="container">
           <div className="row">
-            <h4 className="h4Test" id="aboutMeId">
-              About me
-            </h4>
-            <p>
-              Systemdeveloper(Java), that likes to play around in ReactJS. When
-              Im not developing Im usually skateboarding or punching people in
-              the face(boxing).
-            </p>
+            <div className="col-12">
+              <h4 id="aboutMeId">About me</h4>
+              <p>
+                I am Systemdeveloper(Java) from Stockholm that likes to play
+                around in ReactJS. When Im not developing Im usually
+                skateboarding or punching/get punched by people in the
+                face(boxing). This website is intended for giving you a picture
+                of who I am and my skill set.
+              </p>
+            </div>
           </div>
         </div>
       </article>
